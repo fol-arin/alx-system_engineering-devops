324f7cca57b31b5e65a36128ca71def45e63343c
fol-arin <136228301+fol-arin@users.noreply.github.com>
2024-01-24 22:09:52 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-#Create a file in /tmp
+# Creates a new puppet file
 
-file { 'school':
-ensure  => 'present',
-path    => '/tmp/school',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet'
+file { '/tmp/school':
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
